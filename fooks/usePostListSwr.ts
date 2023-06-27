50dc3d76a875b73ea6eeaeecdd0425ddecbaf6d0
kameiryousuke <kamei.r@i.softbank.jp>
2023-06-27 15:07:11 +0900

    未完成

---
+++ fooks/usePostListSwr.ts
@@ -0,0 +1,19 @@
+import useSWR from 'swr'
+
+
+// service
+;
+import { WpGraphQlPostConst } from "../constants/GraphQlConst";
+import PostService from "../services/PostService";
+import PostType from "../types/PostType";
+
+const usePostListSwr = (staticPostList: PostType[]) => {
+    const { data: postList } = useSWR(
+        WpGraphQlPostConst.list,
+        PostService.getList,
+        { fallbackData: staticPostList }
+    )
+    return postList;
+}
+
+export default usePostListSwr;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,33 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import PostService from '../services/PostService'
+import PostType from '../types/PostType'
 
-const Home: NextPage = () => {
+const Home: NextPage<{
+  staticPostList: PostType[] // 型の指定をする場所に注意！
+}> = ({ staticPostList }) => {
+
   return (
-    <div className="text-red-900">
-      <p className='text-red-900'>kame</p>
+    <div>
+      {staticPostList.map((post) => {
+        console.log(staticPostList)
+        return (
+        <>
+        <p key={post.id}>{post.title}</p> 
+        <p>{post.date}</p>
+        </>
+        )
+      })}
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const staticPostList = await PostService.getList(); // postListをとってくる
+  return {
+    props: {
+      staticPostList
+    }
+  }
 }
 
 export default Home
